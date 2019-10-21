f1c0eb2b1572b8f19e37b598618d71e2b2525365
Hiroki Nagamine <hiroki.nagamine@gmail.com>
2019-10-21 19:51:02 +0900

    ログインフォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'toppages#index'
 
   get 'signup', to: 'users#new' #urlが'signupになる'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users, only: %i[index show new create]
 end
