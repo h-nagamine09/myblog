8b22faedcb73395c282ed09ec8648853026f2b0b
Hiroki Nagamine <hiroki.nagamine@gmail.com>
2019-10-22 22:55:08 +0900

    blogテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191021093731) do
+ActiveRecord::Schema.define(version: 20191022135255) do
+
+  create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title", null: false
+    t.string "content", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 20191021093731) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "users"
 end
